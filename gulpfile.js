03a5090b7485c4899b1a9e806e1370c8faa4c979
zSaylloonn . <eduardo.f5@outlook.com>
2025-07-31 01:04:15 -0300

    add do Hero e Font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
-const gulp = require('gulp');
+const gulp = require('gulp'); //instalar o Gulp 4.0.2
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin'); //instalar o imagemin 7.1.0
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch =function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 }
