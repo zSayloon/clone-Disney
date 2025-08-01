5ed47e88bdc7855628cca006c69192c89fa24b66
zSaylloonn . <eduardo.f5@outlook.com>
2025-08-01 15:13:35 -0300

    Atualização

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSeciton = document.querySelector('.hero');
+    const alturaHero = heroSeciton.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual > alturaHero){
+            ocultaElementosDoHeader();
+        } else{
+            exibirElementosDoHeader();
+        }
+    })
+    
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,7 +25,28 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibirElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento){
+    const classe ='faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
